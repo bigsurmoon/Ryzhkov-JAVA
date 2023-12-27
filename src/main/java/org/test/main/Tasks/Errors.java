3e7bdff3ff8461b9ceddeaf9b03f05fc198cc987
Edward Ryzhkov <85837973+bigsurmoon@users.noreply.github.com>
2023-12-27 10:32:23 +0700

    Update Errors.java

--- src/main/java/org/test/main/Tasks/Errors.java
+++ src/main/java/org/test/main/Tasks/Errors.java
@@ -2,8 +2,6 @@ package org.test.main.Tasks;
 
 import java.util.Scanner;
 
-import static org.test.main.Tasks.SecondTask.taskNumberTwo;
-
 public class Errors {
 
     public static void firstTaskError(Runnable runnable) {
@@ -25,13 +23,11 @@ public class Errors {
     public static int[] getInputArray(int size) {
         Scanner scanner = new Scanner(System.in);
         int[] array = new int[size];
-
         System.out.println("Введите элементы массива: ");
         for (int i = 0; i < size; i++) {
             System.out.print("Элемент " + (i + 1) + ": ");
             array[i] = scanner.nextInt();
         }
-
         return array;
     }
 }
